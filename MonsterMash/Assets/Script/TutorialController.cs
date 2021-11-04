3723ebdeb017d54e996cd818964370d2a9fc975a
ferrel kennedy <ferrelkenkan@gmail.com>
2021-11-04 00:23:40 +0000

    tutorial 1

---
+++ MonsterMash/Assets/Script/TutorialController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+ 
+    private List<GameObject> pages = new List<GameObject>();
+    private GameObject prevButton, nextButton;
+    private int numberOfPages,currentPageIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevButton = gameObject.transform.Find("PrevButton").gameObject;
+        nextButton = gameObject.transform.Find("NextButton").gameObject;
+        GameObject parentPage = gameObject.transform.Find("Pages").gameObject;
+        foreach(Transform child in parentPage.transform)
+        {
+            pages.Add(child.gameObject);
+        }
+
+        numberOfPages = pages.Count - 1;
+        nextButton.SetActive(true);
+        prevButton.SetActive(true);
+        pages[currentPageIndex].SetActive(true);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentPageIndex == 0)
+        {
+            prevButton.SetActive(false);
+        }
+        else
+        {
+            prevButton.SetActive(true);
+        }
+
+        //if (currentPageIndex == numberOfPages)
+        //{
+        //    nextButton.SetActive(false);
+        //}
+        //else
+        //{
+        //    nextButton.SetActive(true);
+        //}
+    }
+
+    public void NextPage()
+    {
+        if(currentPageIndex == numberOfPages)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            pages[currentPageIndex].SetActive(false);
+            currentPageIndex++;
+            pages[currentPageIndex].SetActive(true);
+        }
+        
+    }
+
+    public void PrevPage()
+    {
+        pages[currentPageIndex].SetActive(false);
+        currentPageIndex--;
+        pages[currentPageIndex].SetActive(true);
+    }
+}
